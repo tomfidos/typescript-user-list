1a7df5d46fbeb95ba0608414ab05068ea3601fe6
Tomasz Fidos <fidostomasz@gmail.com>
2023-03-31 19:49:51 +0200

    Added user list production.

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,13 +1,49 @@
-function Users(): JSX.Element {
+import React, { useState } from 'react';
+
+import UserList from './UserList';
+import './Users.css';
+
+interface userData {
+    name: string,
+    id: number,
+}
+
+interface Props {
+    userList: userData[],
+}
+
+
+const Users = (): JSX.Element => {
+
+    const [name, setName] = useState<string>('')
+    const [userList, setUserList] = useState<userData[]>([]);
+
+    const changeInput = (event: React.FormEvent<HTMLInputElement>): void => {
+        setName(event.currentTarget.value);
+    }
+
+    const addUser = (event: React.SyntheticEvent): void => {
+        event.preventDefault();
+
+        const user = {
+            name: name,
+            id: Date.now(),
+        }
+
+        setUserList(userList.concat(user));
+    }
+
     return (
         <div>
             <h1>User list</h1>
-            <form>
-                <input type="text" placeholder="Enter name" />
+            <form onSubmit={event => addUser(event)}>
+                <input type="text" placeholder="Enter name" value={name} onChange={changeInput} />
                 <button type="submit">Add user</button>
             </form>
+            <UserList userList={userList} />
         </div>
     );
 }
 
 export default Users;
+export type { Props };
