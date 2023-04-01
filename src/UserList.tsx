a2a841b789b40af96612a209f94a420f68748e37
Tomasz Fidos <fidostomasz@gmail.com>
2023-04-01 16:38:14 +0200

    Added user removal.

--- src/UserList.tsx
+++ src/UserList.tsx
@@ -3,7 +3,7 @@ import { Props } from './Users';
 
 const UserList = (props: Props): JSX.Element => {
 
-    const userElements = props.userList.map(user => <li key={user.id}>{user.name}</li>);
+    const userElements = props.userList.map(user => <li key={user.id}>{user.name}<span onClick={event => props.removeUser(event, user.id)}>X</span></li>);
 
     return (
         <ul>{userElements}</ul>
